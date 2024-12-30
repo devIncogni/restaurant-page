fd7e7ae63239058ff12ecf1b6348e6023d0b292b
devIncogni <arinkumar10@outlook.com>
2024-12-30 12:16:33 +0530

    - Initilized webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.htm"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.htm" })],
+  module: {
+    rules: [
+      {
+        test: /.\html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /.\css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: "asset/resource",
+      },
+    ],
+  },
+};
