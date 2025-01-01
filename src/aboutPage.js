b5f82d41aba25690b42d980a3972810b39b69a93
devIncogni <arinkumar10@outlook.com>
2025-01-01 14:15:16 +0530

    styled and added functionality to about page

---
+++ src/aboutPage.js
@@ -0,0 +1,61 @@
+function generateContent(container) {
+  container.innerHTML = `
+            
+            <div class="about-info">
+                <h1>About Us</h1>
+                <div class="about-para-container">
+                    <p>
+                        “Fueling Developers, One Cup at a Time.”
+                    </p>
+                    <p>
+                        At Code & Coffee, we understand that behind every line of great code is an even greater cup of
+                        coffee. Built by developers, for developers, our café is more than just a place to grab a drink
+                        -
+                        it's a sanctuary where ideas brew as smoothly as our espresso.
+                    </p>
+                    <p>
+                        From all-nighters to debugging marathons, we know the grind. That's why we craft every cup to
+                        keep
+                        you powered through every sprint, commit, and deployment. Our beans are sourced from the finest
+                        farms and roasted to perfection - because developers deserve more than just instant coffee.
+                    </p>
+                    <div>
+                        <ul class="why-choose-us">
+                            <li>
+                                <p>Why Choose Us?</p>
+                            </li>
+                            <li>
+                                <p> Strong Coffee, Stronger Code - Our blends are designed to keep your mind sharp and
+                                    your
+                                    focus
+                                    unbreakable.</p>
+                            </li>
+                            <li>
+                                <p> Free Wi-Fi & Power Ports - Never run out of charge or connection during your
+                                    coding
+                                    sessions.
+                                </p>
+                            </li>
+                            <li>
+                                <p> Community-Driven - Meet fellow devs, join hackathons, or just vibe in our cozy
+                                    co-working space.</p>
+                            </li>
+                            <li>
+                                <p> Tech Talks & Events - Learn, share, and sip alongside tech enthusiasts and
+                                    industry
+                                    leaders.</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <p>
+                        Whether you're debugging bugs or building the next big app, Code & Coffee is your go-to place
+                        for
+                        quality coffee and even better code vibes.
+                    </p>
+                </div>
+
+            </div>
+    `;
+}
+
+export default generateContent;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import "./home-styles.css";
+import "./about-styles.css";
 import generateHome from "./homePage.js";
+import generateAbout from "./aboutPage.js";
 
 console.log("Hello World");
 
@@ -11,6 +13,12 @@ homeBtn.addEventListener("click", (event) => {
   generateHome(holder);
 });
 
-window.addEventListener("load", () => {
-  generateHome(holder);
+// window.addEventListener("load", () => {
+//   generateHome(holder);
+// });
+
+const abtBtn = document.querySelector(".aboutButton");
+
+abtBtn.addEventListener("click", () => {
+  generateAbout(holder);
 });
